116d4eac7c625864050455ed1bab2daef0fc3d6b
Amir Razmjou <arazmj@gmail.com>
2015-11-26 06:59:22 -0500

    Commented the code

--- src/net/amirrazmjou/decisiontree/Main.java
+++ src/net/amirrazmjou/decisiontree/Main.java
@@ -5,17 +5,23 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
-
     // Q.2
     // The Restaurant data set in Fig. 18.3 (3/2Ed) is the training
     // set. No test set for this data set. Your implementation should
     // reproduce the tree in Fig. 18.6 (3/2Ed).
     // The functions (stated in Java) include:
+    //
+
+    /** given a tree and a data set, return the accuracy (%) of the tree on
+     * the dataset
+     * @param tree the tree
+     * @param dataSet the dataset
+     * @return the accuracy
+     */
     public static float evalDecisionTree(DecisionTreeNode tree,
                                          List<Record> dataSet) {
         return (float) tree.evaluate(dataSet) / dataSet.size() * 100;
     }
-
 
     public static void testDecisionTree(String attrFileName,
                                         String trainFileName,
@@ -68,7 +74,6 @@ public class Main {
                 attributeSet, classes, "");
 
         // print the decision tree
-
         System.out.println(decisionTree);
 
         // print the accuracy of decision tree on the training set
@@ -83,39 +88,59 @@ public class Main {
         }
     }
 
-    // test the restaurant data set
+    /**
+     * test the restaurant data set
+     */
     public static void testRestaurant() {
 
         testDecisionTree("restaurant-attr.txt", "restaurant-train.txt", null);
     }
 
+    /**
+     * test the IDS data set
+     */
     public static void testIds() {
 
         testDecisionTree("ids-attr.txt", "ids-train.txt", "ids-test.txt");
     }
 
+    /**
+     * test the Tic Tac Tow data set
+     */
     public static void testTtt() {
 
         testDecisionTree("ttt-attr.txt", "ttt-train.txt", "ttt-test.txt");
     }
 
-
     // Q.4
+    // We would like the program to learn how to make a move in the tic-tac-toe game.
+    // The nine (target) classes are: top-left, top-middle, ...
+    // The initial nine attributes are the nine squares with x, o, b as values
 
-
-    // given the sizes and file names of the training set and test sets
-    // train2-fname and test2-fname have records with additional attributes
-    // generate a random legal board configuration, assuming x is next to move
-    // the user/teacher enters the "correct" move for x
-    // attributes and class of each record are written to the file
-    // each record should be unique
+    /**
+     * given the sizes and file names of the training set and test sets
+     * train2-fname and test2-fname have records with additional attributes
+     * generate a random legal board configuration, assuming x is next to move
+     * the user/teacher enters the "correct" move for x
+     * attributes and class of each record are written to the file
+     * each record should be unique
+     * @param trainSize
+     * @param trainFileName
+     * @param train2FileName
+     * @param testSize
+     * @param testFileName
+     * @param test2FileName
+     */
     public static void genTttMoveData(int trainSize, String trainFileName, String train2FileName,
-                                      int testSize, String testFileName, String test2FileName) {
+                                      int testSize, String testFileName, String test2FileName)
+    {
 
     }
 
-    // test the ttt data set with initial attributes,
-    // and additional attributes
+    /**
+     * test the ttt data set with initial attributes,
+     * and additional attributes
+     */
     public static void testTttMove() {
         testDecisionTree("ttt-move-attr.txt", "ttt-move-train.txt", "ttt-move-test.txt");
         testDecisionTree("ttt-move-attr2.txt", "ttt-move-train2.txt", "ttt-move-test2.txt");
