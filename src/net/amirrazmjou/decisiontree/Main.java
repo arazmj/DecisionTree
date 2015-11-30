9c9078f6ed700c85825fbc175ad732403aa9f110
Amir Razmjou <arazmj@gmail.com>
2015-11-30 17:37:08 -0500

    Commented the code

--- src/net/amirrazmjou/decisiontree/Main.java
+++ src/net/amirrazmjou/decisiontree/Main.java
@@ -3,10 +3,8 @@ package net.amirrazmjou.decisiontree;
 import net.amirrazmjou.tictactoe.*;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -21,7 +19,7 @@ public class Main {
      * given a tree and a data set, return the accuracy (%) of the tree on
      * the dataset
      *
-     * @param tree    the tree
+     * @param tree the tree
      * @param dataSet the data set
      * @return the accuracy
      */
@@ -123,6 +121,10 @@ public class Main {
         testDecisionTree("ttt-play-attr.txt", "ttt-play-train.txt", "ttt-play-test.txt");
     }
 
+    public static void testTttPlayExtra() {
+        testDecisionTree("ttt-play-extra-attr.txt", "ttt-play-extra-train.txt", "ttt-play-extra-test.txt");
+    }
+
     // Q.4
     // We would like the program to learn how to make a move in the tic-tac-toe game.
     // The nine (target) classes are: top-left, top-middle, ...
@@ -144,56 +146,106 @@ public class Main {
      * @param test2FileName
      */
     public static void genTttMoveData(int trainSize, String trainFileName, String train2FileName,
-                                      int testSize, String testFileName, String test2FileName) throws IOException {
+                                      int testSize, String testFileName, String test2FileName)
+    {
 
-        String[] positions = new String[]
-                {"top-left", "top-middle", "top-right",
-                 "middle-left", "center", "middle-right",
-                 "bottom-left", "bottom-middle", "bottom-right"};
 
         System.out.print("Generating examples...");
 
         // put all training set and test set in a single hash set
         // so we can make sure we have distinct examples
-        HashSet<String> examplesData = new HashSet<>();
+        HashMap<String, String> examplesData = new HashMap<>();
+        HashMap<String, String> opponentMoves = new HashMap<>();
+
         int examplesCount = trainSize + testSize;
         int c = 0;
         while (examplesData.size() < examplesCount) {
             c++;
             Board2D board = new Board2D(3);
             StringBuilder sb = new StringBuilder();
-            putRandomMoves(board, c % 8, c);
-            String beforeMove = board.toSingleLineString();
-            sb.append(beforeMove);
+
+            // we want odd numbers of 1, 3, 5, 7
+            // these are the number of moves that
+            // makes the turn to x on the next move
+            // assuming that it starts with o
+            int n = 2 * (c % 4);
+
+            String beforeMove, afterMove;
+            putRandomMoves(board, n, c);
+            beforeMove = board.toSingleLineString();
+            putRandomMoves(board, 1, c);
+            afterMove = board.toSingleLineString();
+            String opponentMove = findMove(beforeMove, afterMove);
+
+            beforeMove = board.toSingleLineString();
+
+            sb.append(board.toSingleLineString());
 
             Player player = new SimpleMaxMinPlayer(board, Seed.CROSS);
             player.move();
-            String afterMove = board.toSingleLineString();
+            afterMove = board.toSingleLineString();
 
-            String[] splitBeforeMove = beforeMove.split(" ");
-            String[] splitAfterMove = afterMove.split(" ");
+            String move = findMove(beforeMove, afterMove);
 
-            int i = 0;
-            for (; i < splitAfterMove.length; i++) {
-                if (!splitAfterMove[i].equals(splitBeforeMove[i]))
-                    break;
-            }
-
-            sb.append(positions[i]);
-            examplesData.add(sb.toString());
+            examplesData.put(sb.toString(), move);
+            opponentMoves.put(sb.toString(), opponentMove);
 
             if (c % 20 == 0)
                 System.out.print(".");
         }
+        List<Map.Entry<String, String>> examplesEntries = new ArrayList<>(examplesData.entrySet());
 
-        ArrayList<String> arrayList = new ArrayList<>(examplesData);
-        List<String> trainData = arrayList.subList(0, trainSize);
-        List<String> testData = arrayList.subList(trainSize, trainSize + testSize);
+        Collections.shuffle(examplesEntries, new Random(1));
+
+        List<String> lineExamples = examplesEntries
+                .stream()
+                .map(p -> p.getKey()  + p.getValue())
+                .collect(Collectors.toList());
+
+        List<String> trainLines = lineExamples.subList(0, trainSize);
+        List<String> testLines = lineExamples.subList(trainSize, trainSize + testSize);
+
+        Function<String, String> countCross = s -> opponentMoves.get(s); //s.chars().filter(p -> p == 'o' || p == 'x').count();
+
+        List<String> lineExamplesExtra = examplesEntries
+                .stream()
+                .map(p-> p.getKey() + countCross.apply(p.getKey()) + " " + p.getValue())
+                .collect(Collectors.toList());
+
+        List<String> extraValues = examplesEntries
+                .stream()
+                .map(p -> countCross.apply(p.getKey()).toString())
+                .distinct()
+                .collect(Collectors.toList());
+
+        String join = String.join(" ", extraValues);
+        System.out.println();
+        System.out.println(join);
+
+
+        List<String> trainDataExtra = lineExamplesExtra.subList(0, trainSize);
+        List<String> testDataExtra = lineExamplesExtra.subList(trainSize, trainSize + testSize);
 
         try {
             try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(train2FileName), "utf-8"))) {
+                for (String s : trainDataExtra) {
+                    writer.write(s);
+                    writer.write("\n");
+                }
+            }
+
+            try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(test2FileName), "utf-8"))) {
+                for (String s : testDataExtra) {
+                    writer.write(s);
+                    writer.write("\n");
+                }
+            }
+
+            try (Writer writer = new BufferedWriter(new OutputStreamWriter(
                     new FileOutputStream(trainFileName), "utf-8"))) {
-                for (String s : trainData) {
+                for (String s : trainLines) {
                     writer.write(s);
                     writer.write("\n");
                 }
@@ -201,48 +253,64 @@ public class Main {
 
             try (Writer writer = new BufferedWriter(new OutputStreamWriter(
                     new FileOutputStream(testFileName), "utf-8"))) {
-                for (String s : testData) {
+                for (String s : testLines) {
                     writer.write(s);
                     writer.write("\n");
                 }
             }
+
         } catch (IOException ex) {
-            System.out.println("Cannot write the file "  + ex.getMessage());
+            System.out.println("Cannot write the file named " + ex.getMessage());
         }
     }
 
-    public static void putRandomMoves(Board board, int n, int seed) {
+    private static String findMove(String beforeMove, String afterMove) {
+        String[] positions = new String[]
+                {"top-left", "top-middle", "top-right",
+                        "middle-left", "center", "middle-right",
+                        "bottom-left", "bottom-middle", "bottom-right"};
+
+        String[] splitBeforeMove = beforeMove.split(" ");
+        String[] splitAfterMove = afterMove.split(" ");
+
+        int i = 0;
+        for (; i < splitAfterMove.length; i++) {
+            if (!splitAfterMove[i].equals(splitBeforeMove[i]))
+                break;
+        }
+        return positions[i % positions.length];
+    }
+
+    private static void putRandomMoves(Board board, int n, int seed)
+    {
         Random random = new Random();
         random.setSeed(seed);
+        // start with cross
         boolean b = true;
-        for (int x = 0; x < n; x++) {
+        int size1 = board.getAvailableMoves().size();
+        int tries = 0;
+        while (board.getAvailableMoves().size() > size1 - n && tries < 10)
+        {
             List<Point> availableMoves = board.getAvailableMoves();
             int size = availableMoves.size();
             int r = random.nextInt(size);
             board.setCell(availableMoves.get(r), b ? Seed.NOUGHT : Seed.CROSS);
             if (board.winner() != null) {
                 board.setCell(availableMoves.get(r), Seed.EMPTY);
+                tries++;
             } else {
                 b = !b;
             }
         }
     }
 
-    /**
-     * test the ttt data set with initial attributes,
-     * and additional attributes
-     */
-    public static void testTttMove() {
-        testDecisionTree("ttt-move-attr.txt", "ttt-move-train.txt", "ttt-move-test.txt");
-        testDecisionTree("ttt-move-attr2.txt", "ttt-move-train2.txt", "ttt-move-test2.txt");
-    }
-
-    public static void main(String[] args) throws IOException {
-        genTttMoveData(10, "ttt-play-train.txt", "",
-                      200, "ttt-play-test.txt", "");
+    public static void main(String[] args)  {
+        genTttMoveData(200, "ttt-play-train.txt", "ttt-play-extra-train.txt",
+                       50, "ttt-play-test.txt", "ttt-play-extra-test.txt");
 //        testRestaurant();
 //        testIds();
 //        testTtt();
         testTttPlay();
+        testTttPlayExtra();
     }
 }
